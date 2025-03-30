e317e3efef1198c30b172b3b3f427200c45e13a8
Douglascrc <douglasbuscam@gmail.com>
2025-03-30 12:48:22 -0300

    feat: make user avatar field required and upload default avatar to S3

--- backend/src/services/s3-service.ts
+++ backend/src/services/s3-service.ts
@@ -1,8 +1,6 @@
-import {
-  CreateBucketCommand,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import { CreateBucketCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import path from "path";
+import fs from "fs/promises";
 
 const bucketName = process.env.BUCKET_NAME!;
 
@@ -19,6 +17,24 @@ const s3 = new S3Client({
 export async function createBucket() {
   await s3.send(new CreateBucketCommand({ Bucket: bucketName }));
   console.log("Bucket criado com sucesso.");
+  await uploadDefaultAvatar();
+}
+
+export async function uploadDefaultAvatar() {
+  try {
+    const filePath = path.join(process.cwd(), "src", "tests", "mocks", "avatar.png");
+    const fileBuffer = await fs.readFile(filePath);
+    const uploadParams = {
+      Bucket: bucketName,
+      Key: "avatar.png",
+      Body: fileBuffer,
+      ContentType: "image/png",
+    };
+    await s3.send(new PutObjectCommand(uploadParams));
+    console.log("Imagem padrão (avatar.png) enviada para o bucket.");
+  } catch (error) {
+    console.error("Erro ao enviar a imagem padrão:", error);
+  }
 }
 
 export async function uploadImage(file: Express.Multer.File) {
