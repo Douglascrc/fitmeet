ad7718051f34d5cdc3333231c9ada1250f5ab862
Douglascrc <douglasbuscam@gmail.com>
2025-03-30 15:48:01 -0300

    doc: add Swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,12 @@ import userController from "./controllers/user-controller";
 import authController from "./auth/auth-controller";
 import activityController from "./controllers/activity-controller";
 import { createBucket } from "./services/s3-service";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+
+const swaggerPath = path.join(__dirname, "swagger.yaml");
+const swaggerFile = YAML.load(swaggerPath);
 
 export const server = express();
 
@@ -22,6 +28,8 @@ authController(server);
 activityController(server);
 
 const port = process.env.PORT;
+
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
