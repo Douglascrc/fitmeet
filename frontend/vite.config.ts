c02f2c80171f7e1885875e65128ada119cf6604c
Douglascrc <douglasbuscam@gmail.com>
2025-04-11 23:50:46 -0300

    chore: update path aliases and modify index.html title and favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
+import { join } from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": join(__dirname, "./src"),
+    },
+  },
 });
