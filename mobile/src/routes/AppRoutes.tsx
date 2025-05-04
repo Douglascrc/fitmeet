12441abe7ff1fc84890672fb3d3ab540069dfb85
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-05-03 23:43:36 -0300

    feat: add Profile screen to navigation stack

--- mobile/src/routes/AppRoutes.tsx
+++ mobile/src/routes/AppRoutes.tsx
@@ -4,12 +4,14 @@ import Login from "../pages/Login/Login";
 import {NavigationContainer} from "@react-navigation/native";
 import {Register} from "../pages/Register/Register";
 import ActivityCategory from "../pages/ActivityCategory/ActivityCategory";
+import Profile from "../pages/Profile/Profile";
 
 export type MainStackParamList = {
   Login: {name: string; isError: boolean};
   Register: {name: string; isError: boolean};
   Home: {name: string; isError: boolean};
   Activities: {categoryId: string; categoryName: string};
+  Profile: undefined;
 };
 
 const MainStack = createStackNavigator();
@@ -22,6 +24,7 @@ function MainStackScreen() {
         <MainStack.Screen name="Login" component={Login} />
         <MainStack.Screen name="Register" component={Register} />
         <MainStack.Screen name="Activities" component={ActivityCategory} />
+        <MainStack.Screen name="Profile" component={Profile} />
       </MainStack.Group>
     </MainStack.Navigator>
   );
