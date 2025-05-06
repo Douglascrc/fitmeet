240d72afa67aa48f67d9c57ea7f02447a08b73f8
Douglas Campos <60862078+Douglascrc@users.noreply.github.com>
2025-05-05 22:18:18 -0300

    feat: add Toast component for user notifications in App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppRoutes from "./src/routes/AppRoutes";
 import {AppStateProvider} from "./src/context/AppState";
+import Toast from "react-native-toast-message";
 
 function App() {
   return (
     <AppStateProvider>
       <AppRoutes />
+      <Toast autoHide={true} visibilityTime={2000} />
     </AppStateProvider>
   );
 }
