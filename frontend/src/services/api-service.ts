45b5277f3aeac28d75143cace6fbff62acf69f9d
Douglascrc <douglasbuscam@gmail.com>
2025-05-24 23:10:48 -0300

    fix:  baseURL to include '/api'

--- frontend/src/services/api-service.ts
+++ frontend/src/services/api-service.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const auth_api = axios.create({
-  baseURL: "https://fitmeet-back.vercel.app",
+  baseURL: "https://fitmeet-back.vercel.app/api/auth",
 });
 
 export const user_api = axios.create({
-  baseURL: "https://fitmeet-back.vercel.app",
+  baseURL: "https://fitmeet-back.vercel.app/api/user",
 });
 
 export const activities_api = axios.create({
-  baseURL: "https://fitmeet-back.vercel.app",
+  baseURL: "https://fitmeet-back.vercel.app/api/activities",
 });
